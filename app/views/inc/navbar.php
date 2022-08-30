31b741e40bc5895953a94090dc84e25e98cd9305
Armanur Rashid <58313733+Arman13-arch@users.noreply.github.com>
2022-08-30 20:19:59 +0600

    Search Bar

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -7,10 +7,11 @@
 
         <div class="collapse navbar-collapse" id="navbarcollapseCMS">
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a href="<?php echo URLROOT; ?>/pages/home" class="nav-link"><i class="fas fa-house-user"></i> Home</a>
-                </li>
+
                 <?php if (isset($_SESSION['user_id'])) : ?>
+                    <li class="nav-item">
+                        <a href="<?php echo URLROOT; ?>/pages/home" class="nav-link"><i class="fas fa-house-user"></i> Home</a>
+                    </li>
                     <li class="nav-item">
                         <a href="<?php echo URLROOT; ?>/users/profile" class="nav-link"><i class="fas fa-user"></i> My Profile</a>
                     </li>
@@ -18,9 +19,21 @@
                         <a href="<?php echo URLROOT; ?>/posts" class="nav-link"><i class="fa-solid fa-sim-card"></i> Posts</a>
                     </li>
                     <li class="nav-item">
-                        <a href="<?php echo URLROOT; ?>/pages/categories" class="nav-link">Categories</a>
+                        <a href="<?php echo URLROOT; ?>/pages/categories" class="nav-link"><i class="fa-solid fa-layer-group"></i> Categories</a>
                     </li>
+
+                    <div style="display:flex ;">
+                        <li style="margin-top: 5px;">
+                            <input class="form-control form-control-sm bg-light" type="search" placeholder="Search" aria-label="Search">
+                        </li>
+                        <li style=" margin-top: 7px;">
+                            <button class="btn btn-outline-light search-icon" type="submit" style="height: 32px;display:flex;align-items: flex-end;margin-left: 8px;"><i class="fa-solid fa-magnifying-glass"></i></button>
+                        </li>
+                    </div>
                 <?php else : ?>
+                    <li class="nav-item">
+                        <a href="<?php echo URLROOT; ?>/pages/index" class="nav-link"><i class="fas fa-house-user"></i> Home</a>
+                    </li>
                     <li class="nav-item">
                         <a href="<?php echo URLROOT; ?>/pages/about" class="nav-link"><i class="fas fa-user"></i> About</a>
                     </li>
@@ -28,9 +41,6 @@
             </ul>
             <ul class="navbar-nav ms-auto">
                 <?php if (isset($_SESSION['user_id'])) : ?>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"><?php echo $_SESSION['user_name']; ?></a>
-                    </li>
                     <li class="nav-item">
                         <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout"><i class="fa-solid fa-right-from-bracket"> Logout</i></a>
                     </li>
