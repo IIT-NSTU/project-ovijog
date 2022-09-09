0f9f010832ba38f061706fa9166dd8e5517a3665
arnabdey24 <arnabdey665@gmail.com>
2022-09-09 06:46:35 +0600

    post loads according to scrolling

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -17,52 +17,23 @@
     </div>
 
     <div class="container mt-2 mb-2">
-        <div class="row d-flex align-items-center justify-content-center">
+        <div id="data" class="row d-flex align-items-center justify-content-center">
 
-            <?php foreach ($data['posts'] as $post) : ?>
-                <div class="col-md-8 mb-3">
-                    <div class="card">
-                        <div class="d-flex justify-content-between p-2 px-3">
-                            <div class="d-flex flex-row align-items-center"> <img src="https://bootdey.com/img/Content/avatar/avatar7.png" width="40" class="rounded-circle">
-                                <div class="d-flex flex-column"> <span class="font-weight-bold"><?php echo $post->title; ?> </span>
-                                    <small class="text-primary">Category: <?php echo $post->category; ?></small>
-                                </div>
-                            </div>
-                            <div>
-                                <div class="d-flex flex-row mt-1 ellipsis"> <small class="mr-2"><?php echo $post->created_time; ?></small></div>
-                                <div class="text-muted"><small><i class="fa-solid fa-eye"></i> <?php echo $data['view-count'][$post->post_id]; ?> views</small></div>
-                                <?php if ($post->issolved) : ?>
-                                    <div class="text-success"><i class="fa-solid fa-circle-check"></i> Solved</div>
-                                <?php endif; ?>
-                            </div>
-                        </div>
-                        <div class="text-center">
-                            <img src="<?php echo $post->img_link; ?>" width="50%" height="auto" class="img-fluid text-center py-3">
-                        </div>
-                        <div class="px-5 py-2">
-                            <p class="body-text"><?php echo $post->body; ?></p>
-                            <hr>
-                            <div class="row ">
-
-                                <div class="col-sm up-down-vote-icon "><b id="up-count"><?php echo $data['up-count'][$post->post_id]; ?></b><a id="up" onclick="like(this.parentNode.parentNode,<?php echo $post->post_id; ?>)" class="btn btn-sm <?php echo (empty($data['up-votes'][$post->post_id])) ? 'btn-outline-success' : 'btn-success'; ?> ms-2"><i class="fa-solid fa-arrow-up "></i></a></div>
-                                <div class="col-sm up-down-vote-icon"><b id="down-count"><?php echo $data['down-count'][$post->post_id]; ?></b><a id="down" onclick="dislike(this.parentNode.parentNode,<?php echo $post->post_id; ?>)" class="btn btn-sm <?php echo (empty($data['down-votes'][$post->post_id])) ? 'btn-outline-danger' : 'btn-danger'; ?> ms-2"><i class="fa-solid fa-arrow-down"></i> </a></div>
-
-                                <?php if ($post->user_id != $_SESSION['user_id']) : ?>
-                                    <div class="col-sm text-center"><a href="#" class="btn btn-sm text-danger"><b>Report</b></a></div>
-                                <?php endif; ?>
-
-                                <div class="col-sm text-center"><a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->post_id; ?>" class="btn btn-sm text-primary"><b>More</b></a></div>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            <?php endforeach; ?>
+           <!-- All post here ----->
 
         </div>
     </div>
 
 </div>
+
+    <!-------spinner------->
+    <div class="d-flex justify-content-center">
+        <div id="loading" class="spinner-border m-5" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
+    </div>
+    <!-------spinner end------->
+
 <script>
     function like(d, id) {
 
@@ -116,6 +87,34 @@
             }
         });
     }
+
+    var page_no=1;
+    var isrunning=false;
+    var halt=false;
+    showMore();
+
+    $(window).scroll(function () {
+        if($(window).scrollTop()+$(window).height() > $(document).height()-100){
+           if(!isrunning && !halt){
+               showMore();
+           }
+        }
+    });
+
+    function showMore() {
+        isrunning=true;
+        $('#loading').show();
+        $.post('<?php echo URLROOT; ?>/posts/load/'+page_no,{page:page_no},(response)=>{
+            if(response===""){
+                halt=true;
+            }
+            $('#data').append(response);
+            $('#loading').hide();
+            isrunning=false;
+            page_no++;
+        });
+    }
+
 </script>
 
 
---
+++ app/views/posts/post.php
@@ -0,0 +1,37 @@
+<div class="col-md-8 mb-3">
+                    <div class="card">
+                        <div class="d-flex justify-content-between p-2 px-3">
+                            <div class="d-flex flex-row align-items-center"> <img src="https://bootdey.com/img/Content/avatar/avatar7.png" width="40" class="rounded-circle">
+                                <div class="d-flex flex-column"> <span class="font-weight-bold"><?php echo $post->title; ?> </span>
+                                    <small class="text-primary">Category: <?php echo $post->category; ?></small>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="d-flex flex-row mt-1 ellipsis"> <small class="mr-2"><?php echo $post->created_time; ?></small></div>
+                                <div class="text-muted"><small><i class="fa-solid fa-eye"></i> <?php echo $data['view-count'][$post->post_id]; ?> views</small></div>
+                                <?php if ($post->issolved) : ?>
+                                    <div class="text-success"><i class="fa-solid fa-circle-check"></i> Solved</div>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                        <div class="text-center">
+                            <img src="<?php echo $post->img_link; ?>" width="50%" height="auto" class="img-fluid text-center py-3">
+                        </div>
+                        <div class="px-5 py-2">
+                            <p class="body-text"><?php echo $post->body; ?></p>
+                            <hr>
+                            <div class="row ">
+
+                                <div class="col-sm up-down-vote-icon "><b id="up-count"><?php echo $data['up-count'][$post->post_id]; ?></b><a id="up" onclick="like(this.parentNode.parentNode,<?php echo $post->post_id; ?>)" class="btn btn-sm <?php echo (empty($data['up-votes'][$post->post_id])) ? 'btn-outline-success' : 'btn-success'; ?> ms-2"><i class="fa-solid fa-arrow-up "></i></a></div>
+                                <div class="col-sm up-down-vote-icon"><b id="down-count"><?php echo $data['down-count'][$post->post_id]; ?></b><a id="down" onclick="dislike(this.parentNode.parentNode,<?php echo $post->post_id; ?>)" class="btn btn-sm <?php echo (empty($data['down-votes'][$post->post_id])) ? 'btn-outline-danger' : 'btn-danger'; ?> ms-2"><i class="fa-solid fa-arrow-down"></i> </a></div>
+
+                                <?php if ($post->user_id != $_SESSION['user_id']) : ?>
+                                    <div class="col-sm text-center"><a href="#" class="btn btn-sm text-danger"><b>Report</b></a></div>
+                                <?php endif; ?>
+
+                                <div class="col-sm text-center"><a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->post_id; ?>" class="btn btn-sm text-primary"><b>More</b></a></div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
