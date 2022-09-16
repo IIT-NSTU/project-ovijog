2a1d010feae35d77aae7f6a568aff39eac5eb46a
Armanur Rashid <58313733+Arman13-arch@users.noreply.github.com>
2022-09-16 21:25:51 +0600

    Admin Manage Category

--- app/views/admins/manageCategory.php
+++ app/views/admins/manageCategory.php
@@ -2,12 +2,60 @@
 <?php require_once APPROOT . '/views/inc/navbar.php'; ?>
 <?php require_once APPROOT . '/views/inc/sidebar.php'; ?>
 <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/sidebar.css">
+<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/admin.css">
 
 <body>
-
-    <div class="main" style="margin-left:173px;">
-        <h2>Manage Category</h2>
-
+    <div class="main py-5" style="margin-left:173px;">
+        <div class="card" style="width: 65%; margin-left:19%;">
+            <div class="card-header">
+                <div class="row">
+                    <div class="col col-md-6">
+                        <i class="fas fa-table me-1"></i> <span class="table-head">Manage Category</span>
+                    </div>
+                    <div class="col col-md-6" align="right">
+                        <a href="author.php?action=add" class="btn btn-success btn-sm">Add Category</a>
+                    </div>
+                </div>
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered my-4">
+                    <thead>
+                        <tr class="text-center">
+                            <th>#</th>
+                            <th>Category</th>
+                            <th style="width: 20%;">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="text-center">
+                            <td>1</td>
+                            <td>Water</td>
+                            <td class="d-flex justify-content-around"><button class="btn btn-sm btn-danger">Delete</button></td>
+                        </tr>
+                        <tr class="text-center">
+                            <td>2</td>
+                            <td>Food</td>
+                            <td class="d-flex justify-content-around"><button class="btn btn-sm btn-danger">Delete</button></td>
+                        </tr>
+                        <tr class="text-center">
+                            <td>3</td>
+                            <td>Transport</td>
+                            <td class="d-flex justify-content-around"><button class="btn btn-sm btn-danger">Delete</button></td>
+                        </tr>
+                        <tr class="text-center">
+                            <td>4</td>
+                            <td>Security</td>
+                            <td class="d-flex justify-content-around"><button class="btn btn-sm btn-danger">Delete</button></td>
+                        </tr>
+                        <tr class="text-center">
+                            <td>5</td>
+                            <td>Residence</td>
+                            <td class="d-flex justify-content-around"><button class="btn btn-sm btn-danger">Delete</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </body>
 
--- app/views/admins/managePost.php
+++ app/views/admins/managePost.php
@@ -2,15 +2,16 @@
 <?php require_once APPROOT . '/views/inc/navbar.php'; ?>
 <?php require_once APPROOT . '/views/inc/sidebar.php'; ?>
 <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/sidebar.css">
+<link rel="stylesheet" href="<?php echo URLROOT; ?>/css/admin.css">
 
 <body>
     <div class="main py-5" style="margin-left:173px;">
-        <!-- <div class=""> -->
         <div class="card">
             <div class="card-header">
                 <div class="row">
                     <div class="col col-md-6">
-                        <i class="fas fa-table me-1"></i> Manage post
+                        <i class="fas fa-table me-1"></i>
+                        <span class="table-head">Manage post</span>
                     </div>
                     <!-- <div class="col col-md-6" align="right">
                             <a href="author.php?action=add" class="btn btn-success btn-sm">Add</a>
@@ -80,7 +81,6 @@
                 </table>
             </div>
         </div>
-        <!-- </div> -->
     </div>
 </body>
 
