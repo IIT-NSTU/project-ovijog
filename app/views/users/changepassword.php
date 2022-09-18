e8ec84f6196c8700844f7d8d205dced434aedf1e
arnabdey24 <arnabdey665@gmail.com>
2022-09-18 21:39:42 +0600

    redundant file removed

--- app/views/users/changepassword.php
+++
@@ -1,8 +0,0 @@
-<?php require APPROOT . '/views/inc/header.php'; ?>
-
-<!--style here-->
-
-<?php require_once APPROOT . '/views/inc/navbar.php'; ?>
-
-
-<?php require APPROOT . '/views/inc/footer.php'; ?>
